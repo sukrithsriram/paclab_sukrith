ef216603eaa6369582d11a1666edbe90460bd32e
Sukrith Sriram Vedapuram <146099334+sukrithsriram@users.noreply.github.com>
2024-03-17 20:17:50 -0400

    Create triggertest.py

---
+++ Poke Detection/triggertest.py
@@ -0,0 +1,26 @@
+import pigpio
+
+a_state = 0
+count = 0
+
+def poke_detected(pin, level, tick): #  Callback function triggered when pulse A is detected.
+    global a_state, count
+    a_state = 1
+    count += 1
+    print("Poke Detected!")
+    print("Poke Count: ", count)
+
+def poke_down(pin, level, tick): # Callback function triggered when pulse A goes down.
+    global a_state
+    a_state = 0
+    print("Poke Completed")
+
+pi = pigpio.pi()
+pi.callback(12, pigpio.RISING_EDGE, poke_detected)
+pi.callback(12, pigpio.FALLING_EDGE, poke_down)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pi.stop()
