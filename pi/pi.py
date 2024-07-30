c2e6275c520908b234794ae530051417535ca44b
PAC Lab <rodgers.paclab@gmail.com>
2024-07-30 11:18:57 -0400

    error fix

--- pi/pi.py
+++ pi/pi.py
@@ -249,7 +249,7 @@ class SoundQueue:
         # Fill the queue with empty frames
         # Sounds aren't initialized till the trial starts
         # Using False here should work even without sounds initialized yet
-        self.initialize_sounds(self.blocksize, self.fs, self.target_amplitude, self.target_highpass,  self.target_lowpass)
+        self.initialize_sounds(self.blocksize, self.fs, self.amplitude, self.target_highpass,  self.target_lowpass)
         self.set_sound_cycle(params={'left_on': self.left_on, 'right_on': self.right_on})
 
         # Use this to keep track of generated sounds
