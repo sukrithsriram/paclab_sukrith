422f1d6863e5329999246e2dbc89b01b8810083c
PAC Lab <rodgers.paclab@gmail.com>
2024-07-31 11:04:10 -0400

    error fix

--- pi/pi.py
+++ pi/pi.py
@@ -1028,6 +1028,8 @@ try:
                 sound_chooser.update_parameters(
                     rate_min, rate_max, irregularity_min, irregularity_max, 
                     amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
+                sound_chooser.empty_queue()
+
                 #sound_chooser.set_sound_cycle()
                 
                 # Turn off the currently active LED
