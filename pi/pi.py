47a056dfe775dc9fe2d4be62d587efd2cff340e9
PAC Lab <rodgers.paclab@gmail.com>
2024-07-31 15:58:35 -0400

    trying to fix cycle

--- pi/pi.py
+++ pi/pi.py
@@ -890,6 +890,7 @@ try:
             sound_chooser.update_parameters(
                 rate_min, rate_max, irregularity_min, irregularity_max, 
                 amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
+            sound_chooser.initialize_sounds()
             sound_chooser.set_sound_cycle()
             
             # Debug print
