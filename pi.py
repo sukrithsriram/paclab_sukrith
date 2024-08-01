fe0a7a6e84b155f3efe7da24ff775678d2bbe929
PAC Lab <rodgers.paclab@gmail.com>
2024-08-01 13:04:25 -0400

    fixing CSV saving

--- pi.py
+++ pi.py
@@ -1046,7 +1046,7 @@ try:
                 # TODO: fix this; rate_min etc are not necessarily defined
                 # yet, or haven't changed recently
                 # Reset play mode to 'none'
-                new_ params = sound_chooser.update_parameters(
+                new_params = sound_chooser.update_parameters(
                     rate_min, rate_max, irregularity_min, irregularity_max, 
                     amplitude_min, amplitude_max, center_freq_min, center_freq_max, bandwidth)
                 poke_socket.send_string(new_params)
